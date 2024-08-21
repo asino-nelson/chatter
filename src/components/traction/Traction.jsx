76355fdd5c0f778937d17335f54600e199a528b1
asino-nelson <asinonelson276@gmail.com>
2024-08-21 10:52:39 +0300

    Traction section

--- src/components/traction/Traction.jsx
+++ src/components/traction/Traction.jsx
@@ -1,11 +1,30 @@
-
-
 const Traction = () => {
   return (
-    <div>
-      Traction
+    <div className="bg-[#EDE7EF] w-full h-[554px] p-10">
+      <div className="py-10 px-16">
+        <div className="flex flex-row justify-between">
+          <div className="text-[#3C053C]">
+            <h2 className="text-4xl font-semibold">Success Stories</h2>
+            <p className="pr-20">
+              We help businesses convert conversations to results. Join this
+              list of businesses that we work with.
+            </p>
+            <a href="#">Read the latest stories</a>
+          </div>
+          <div className="pr-32">
+            <div className=" grid grid-cols-3 pr-32">
+              <img src="./images/Link_margin.png" alt="image" />
+              <img src="./images/ford.png" alt="image" />
+              <img src="./images/audi.png" alt="image" />
+              <img src="./images/samsung.png" alt="image" />
+              <img src="./images/ikea.png" alt="image" />
+              <img src="./images/pinterest.png" alt="image" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Traction
+export default Traction;
