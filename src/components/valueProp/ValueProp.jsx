500b642d4b9db91663ec47384edddfd2a36d0701
asino-nelson <asinonelson276@gmail.com>
2024-08-21 10:48:25 +0300

    Value prop section

--- src/components/valueProp/ValueProp.jsx
+++ src/components/valueProp/ValueProp.jsx
@@ -1,11 +1,15 @@
-
+import ValuePropCard from "./ValuePropCard";
 
 const ValueProp = () => {
   return (
-    <div>
-      ValueProp
+    <div id="about" className="bg-[#F6F3F7] w-full h-[534px] p-10">
+      <div className="py-8 px-16 gap-4">
+        <h2 className="font-semibold text-3xl">WHY US</h2>
+        <p>Why does your business need ChatterBot?</p>
+      </div>
+      <ValuePropCard />
     </div>
-  )
-}
+  );
+};
 
-export default ValueProp
+export default ValueProp;
