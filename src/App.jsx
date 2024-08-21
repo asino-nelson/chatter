8cccf0dcf37a1f6580d9cd1e954441f0debb1deb
asino-nelson <asinonelson276@gmail.com>
2024-08-21 11:19:35 +0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Footer from "./components/footer/Footer";
+import NavBar from "./components/navbar/NavBar";
 import Landing from "./pages/landing/Landing";
 
 export default function App() {
+  
+  const [selectedPage, setSelectedPage] = useState("/");
+
   return (
     <>
-      <Landing/>
-      <Footer/>
+      <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Landing />
+      <Footer />
     </>
-  )
+  );
 }
