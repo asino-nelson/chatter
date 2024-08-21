6f0ba28b418421a316f73d8a5d9ab481e5c7fff4
asino-nelson <asinonelson276@gmail.com>
2024-08-21 12:11:11 +0300

    App routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from "react";
 import Footer from "./components/footer/Footer";
 import NavBar from "./components/navbar/NavBar";
 import Landing from "./pages/landing/Landing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PrivacyPolicy from "./pages/policy/PrivacyPolicy";
+import TermsOfService from "./pages/policy/TermsOfService";
 
 export default function App() {
-  
   const [selectedPage, setSelectedPage] = useState("/");
 
   return (
-    <>
+    <BrowserRouter>
       <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-of-service" element={<TermsOfService />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
