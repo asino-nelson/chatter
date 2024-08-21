e4d4f6444ba9236b601d4021997c52bdc6dee46b
asino-nelson <asinonelson276@gmail.com>
2024-08-21 17:16:11 +0300

    Footer logo update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,23 +5,23 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="lg:px-48 px-4 md:pt-20 pt-10 pb-10 md:text-xl text-lg flex flex-col shadow-x min-h-[479px] bg-[#F7F7F7]">
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 pb-16 ">
-        <div className="col-span-1">
-          <img src="./images/logo.png" className="w-3/4" alt="logo" />
+    <div className="lg:px-48 px-4 w-full md:pt-20 pt-10 pb-10 md:text-xl text-lg flex flex-col shadow-x min-h-[479px] bg-[#F7F7F7]">
+      <div className="w-full flex md:flex-row flex-col pb-16 ">
+        <div className="flex flex-col md:items-start items-center md:justify-center w-full md:mb-0 mb-10 ">
+          <img src="./images/logo.png" className="md:w-3/4 w-1/2 md:ml-0 ml-16" alt="logo" />
           <p className="mt-4 mb-2 font-semibold">Follow Us</p>
           <div className="flex items-center md:gap-6 gap-4">
-            <FaFacebookSquare />
+            <FaFacebookSquare size={24} />
             {/* <FaSquareXTwitter /> */}
-            <FaLinkedin />
-            <FaInstagram />
+            <FaLinkedin size={24} />
+            <FaInstagram size={24}/>
           </div>
         </div>
 
-        <div className="col-span-3 grid grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-2 lg:grid-cols-3 w-full">
           <div className="flex flex-col">
-            <p className="font-semibold mb-2">Quick Links</p>
-            <div className="flex flex-col gap-y-3">
+            <p className="font-bold mb-2">Quick Links</p>
+            <div className="flex flex-col gap-y-3 text-base font-semibold">
               <FooterLink page="about" label="About us" />
               <FooterLink page="products" label="Products" />
               <FooterLink page="blog" label="Latest News" />
@@ -29,8 +29,8 @@ const Footer = () => {
           </div>
 
           <div className="flex flex-col">
-            <p className="font-semibold mb-2">Products</p>
-            <div className="flex flex-col gap-y-3">
+            <p className="font-bold mb-2">Products</p>
+            <div className="flex flex-col gap-y-3 text-base font-semibold">
               <a href="#">Scheduling</a>
               <a href="#">FAQs</a>
               <a href="#">Callback Request</a>
@@ -39,7 +39,7 @@ const Footer = () => {
           </div>
 
           <div className="flex flex-col">
-            <p className="font-semibold mb-2">Talk to us</p>
+            <p className="font-bold mb-2">Talk to us</p>
             <a href="#">info@chatterbot.com</a>
           </div>
         </div>
