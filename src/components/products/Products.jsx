e69d7556f56af4c4e9bea40ce0404d84376895e8
asino-nelson <asinonelson276@gmail.com>
2024-08-21 14:01:54 +0300

    Product typo

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -3,8 +3,8 @@ import ProductCard from "./ProductCard";
 const Products = () => {
   return (
     <div id="products" className="w-full py-10">
-      <div className="py-8 md:px-16 px-8">
-        <h2 className="text-3xl md:mb-0 mb-3">OUR PRODUCTS</h2>
+      <div className="py-8 md:px-16 px-12">
+        <h2 className="text-3xl font-semibold md:mb-0 mb-3">OUR PRODUCTS</h2>
         <p>Take a closer look at the products we offer.</p>
       </div>
       <div className="md:px-8 px-4 w-full">
