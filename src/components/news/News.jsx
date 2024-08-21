c5770c7b33bf48897258fab6cc777be3f7e421ed
asino-nelson <asinonelson276@gmail.com>
2024-08-21 10:54:42 +0300

    News section

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,10 +1,17 @@
+import NewsItem from "./NewsItem";
 
 const News = () => {
   return (
-    <div>
-      News
+    <div id="blog" className="py-10 gap-3 w-full">
+      <div className="py-8 px-16">
+        <h2 className="font-semibold text-4xl mb-4">LATEST NEWS</h2>
+        <p>Catch up with the latest news, tips and tricks from our team.</p>
+      </div>
+      <div className="px-8">
+        <NewsItem />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
