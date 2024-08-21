b5eaffb092069d8f1c443754d8a02534a32b3832
asino-nelson <asinonelson276@gmail.com>
2024-08-21 11:13:34 +0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Footer from "./components/footer/Footer";
 import Landing from "./pages/landing/Landing";
 
 export default function App() {
   return (
     <>
       <Landing/>
+      <Footer/>
     </>
   )
 }
