1e05bcede59cb8e390d9a971096168eff0e47da3
asino-nelson <asinonelson276@gmail.com>
2024-08-21 10:51:01 +0300

    Products section

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,11 +1,17 @@
-
+import ProductCard from "./ProductCard";
 
 const Products = () => {
   return (
-    <div>
-      Products
+    <div id="products" className="h-[549px] w-full py-10">
+      <div className="py-8 px-16">
+        <h2 className="text-4xl">OUR PRODUCTS</h2>
+        <p className="text-2xl">Take a closer look at the products we offer.</p>
+      </div>
+      <div className="px-8 gap-5 w-full">
+        <ProductCard />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
